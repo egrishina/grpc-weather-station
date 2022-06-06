f18e1b17f68f7124f965622035702e51ab2f2c2a
Екатерина Адеева <catherine.a2402@gmail.com>
2022-06-07 00:02:03 +0200

    Add project

---
+++ src/Route256.WeatherSensorService/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace Route256.WeatherSensorService;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/",
+                async context => { await context.Response.WriteAsync("Hello World!"); });
+        });
+    }
+}
